763d72f9d5f6884eaf610e94cc20fd351705bbd5
preetsimon <preetsimon@gmail.com>
2023-04-13 01:02:45 -0700

    final sol

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Bulb from './components/Bulb'
 
 
 function App() {
-  const [lightclass1, setLight1] = useState({ class: "on", id: 1 })
-  const [lightclass2, setLight2] = useState({ class: "on", id: 2 })
-  const [lightclass3, setLight3] = useState({ class: "on", id: 3 })
-  const [lightclass4, setLight4] = useState({ class: "on", id: 4 })
-  const [lightclass5, setLight5] = useState({ class: "on", id: 5 })
-  const [lightclass6, setLight6] = useState({ class: "on", id: 6 })
-  const [lightclass7, setLight7] = useState({ class: "on", id: 7 })
+  const [lightclass1, setLight1] = useState({ class: "on", id: 0 })
+  const [lightclass2, setLight2] = useState({ class: "on", id: 1 })
+  const [lightclass3, setLight3] = useState({ class: "on", id: 2 })
+  const [lightclass4, setLight4] = useState({ class: "on", id: 3 })
+  const [lightclass5, setLight5] = useState({ class: "on", id: 4 })
+  const [lightclass6, setLight6] = useState({ class: "on", id: 5 })
+  const [lightclass7, setLight7] = useState({ class: "on", id: 6 })
 
 
   const [queue, setQueue] = useState([])
@@ -21,9 +21,12 @@ function App() {
   function enqueueFront(value) {
     setQueue([value, ...queue]);
   }
-  // Adds a new item to the back of the queue.
+  // Adds a new item to the back of the queue.// dont add duplicates
   function enqueueBack(value) {
-    setQueue([...queue, value]);
+
+    if (!queue.includes(value)) {
+      setQueue([...queue, value]);
+    }
   }
   // Removes an item from the front of the queue.
   function dequeueFront() {
@@ -45,6 +48,22 @@ function App() {
     setQueue(queue.slice(0, -1));
     return back;
   }
+
+  // remove bulb fromm queue
+  // const removeMe = (index) => {
+  // const temp = [...queue]
+  // temp.splice(index, 1)
+  // setQueue(temp)
+  //   console.log(`removing ${index}`)
+  // }
+  const removeMe = (index) => {
+    setQueue(
+      queue.filter(el => el !== index)
+    )
+    console.log(`removing ${index}`)
+  }
+
+
   useEffect(() => {
     function forEachWithDelay(array, callback, delay) {
       let i = 0;
@@ -59,20 +78,20 @@ function App() {
         console.log("Queue is full")
         console.log(`turning ${queueItem} on`)
 
-        if (queueItem === 1) {
-          setLight1({ class: "on", id: 1 })
+        if (queueItem === 0) {
+          setLight1({ class: "on", id: 0 })
+        } else if (queueItem === 1) {
+          setLight2({ class: "on", id: 1 })
         } else if (queueItem === 2) {
-          setLight2({ class: "on", id: 2 })
+          setLight3({ class: "on", id: 2 })
         } else if (queueItem === 3) {
-          setLight3({ class: "on", id: 3 })
+          setLight4({ class: "on", id: 3 })
         } else if (queueItem === 4) {
-          setLight4({ class: "on", id: 4 })
+          setLight5({ class: "on", id: 4 })
         } else if (queueItem === 5) {
-          setLight5({ class: "on", id: 5 })
+          setLight6({ class: "on", id: 5 })
         } else if (queueItem === 6) {
-          setLight6({ class: "on", id: 6 })
-        } else if (queueItem === 7) {
-          setLight7({ class: "on", id: 7 })
+          setLight7({ class: "on", id: 6 })
         }
       }, 1000);
 
@@ -80,44 +99,6 @@ function App() {
     console.log(queue)
   }, [queue])
 
-
-
-  // useEffect(() => {
-  //   setBulbQueue(
-  //     (oldQueue) => [...oldQueue, lightclass1]
-  //   )
-  // }, [lightclass1])
-  // useEffect(() => {
-  //   setBulbQueue(
-  //     (oldQueue) => [...oldQueue, lightclass2]
-  //   )
-  // }, [lightclass2])
-  // useEffect(() => {
-  //   setBulbQueue(
-  //     (oldQueue) => [...oldQueue, lightclass3]
-  //   )
-  // }, [lightclass3])
-  // useEffect(() => {
-  //   setBulbQueue(
-  //     (oldQueue) => [...oldQueue, lightclass4]
-  //   )
-  // }, [lightclass4])
-  // useEffect(() => {
-  //   setBulbQueue(
-  //     (oldQueue) => [...oldQueue, lightclass5]
-  //   )
-  // }, [lightclass5])
-  // useEffect(() => {
-  //   setBulbQueue(
-  //     (oldQueue) => [...oldQueue, lightclass6]
-  //   )
-  // }, [lightclass6])
-  // useEffect(() => {
-  //   setBulbQueue(
-  //     (oldQueue) => [...oldQueue, lightclass7]
-  //   )
-  //   console.log(bulbLightQueue)
-  // }, [[lightclass1, lightclass2, lightclass3, lightclass4, lightclass5, lightclass6, lightclass7]])
 
 
   return (
@@ -129,19 +110,19 @@ function App() {
       <h1 style={{ top: "2%", position: 'absolute', left: "30%" }}>Turn Off the lights!</h1>
       <div style={{ position: "absolute", top: "150px", right: "250px", left: "0px", padding: "10px" }} className="wrapper">
 
-        <Bulb toggleBulb={setLight1} lightclass={lightclass1.class} addToQueue={enqueueBack} bulbId={lightclass1.id}></Bulb>
+        <Bulb toggleBulb={setLight1} lightclass={lightclass1.class} addToQueue={enqueueBack} bulbId={lightclass1.id} remove={removeMe}></Bulb>
 
-        <Bulb toggleBulb={setLight2} lightclass={lightclass2.class} addToQueue={enqueueBack} bulbId={lightclass2.id}></Bulb>
+        <Bulb toggleBulb={setLight2} lightclass={lightclass2.class} addToQueue={enqueueBack} bulbId={lightclass2.id} remove={removeMe}></Bulb>
 
-        <Bulb toggleBulb={setLight3} lightclass={lightclass3.class} addToQueue={enqueueBack} bulbId={lightclass3.id}></Bulb>
+        <Bulb toggleBulb={setLight3} lightclass={lightclass3.class} addToQueue={enqueueBack} bulbId={lightclass3.id} remove={removeMe}></Bulb>
 
-        <Bulb toggleBulb={setLight4} lightclass={lightclass4.class} addToQueue={enqueueBack} bulbId={lightclass4.id}></Bulb>
+        <Bulb toggleBulb={setLight4} lightclass={lightclass4.class} addToQueue={enqueueBack} bulbId={lightclass4.id} remove={removeMe}></Bulb>
 
-        <Bulb toggleBulb={setLight5} lightclass={lightclass5.class} addToQueue={enqueueBack} bulbId={lightclass5.id}></Bulb>
+        <Bulb toggleBulb={setLight5} lightclass={lightclass5.class} addToQueue={enqueueBack} bulbId={lightclass5.id} remove={removeMe}></Bulb>
 
-        <Bulb toggleBulb={setLight6} lightclass={lightclass6.class} addToQueue={enqueueBack} bulbId={lightclass6.id}></Bulb>
+        <Bulb toggleBulb={setLight6} lightclass={lightclass6.class} addToQueue={enqueueBack} bulbId={lightclass6.id} remove={removeMe}></Bulb>
 
-        <Bulb toggleBulb={setLight7} lightclass={lightclass7.class} addToQueue={enqueueBack} bulbId={lightclass7.id}></Bulb>
+        <Bulb toggleBulb={setLight7} lightclass={lightclass7.class} addToQueue={enqueueBack} bulbId={lightclass7.id} remove={removeMe}></Bulb>
       </div >
     </>
 
--- src/components/Bulb.jsx
+++ src/components/Bulb.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import "./Bulb.css"
 
 
-const Bulb = ({ lightclass, toggleBulb, addToQueue, bulbId }) => {
+const Bulb = ({ lightclass, toggleBulb, addToQueue, bulbId, remove }) => {
 
 
     const handleClick = () => {
-        lightclass === "on" ? toggleBulb(lightclass => ({
-            ...lightclass, ...{ class: "off" }
-        })) : toggleBulb(lightclass => ({
-            ...lightclass, ...{ class: "on" }
-        }))
+        if (lightclass === "on") {
+            toggleBulb(lightclass => ({
+                ...lightclass, ...{ class: "off" }
+            }))
+            addToQueue(bulbId)
+        } else {
+            toggleBulb(lightclass => ({
+                ...lightclass, ...{ class: "on" }
 
-        lightclass == "on" && addToQueue(bulbId)
-
-
+            }))
+            remove(bulbId)
+        }
     }
     return (
         <div onClick={handleClick} className={"light-bulb " + (lightclass || "")}>
